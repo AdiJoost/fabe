c0fd861797612df935ab8b1372701b4a7eb8fcdf
Adi Joost <adi.joost@gmail.com>
2021-08-22 16:55:58 +0200

    css-file displays models as flexblocks

--- static/js/model.js
+++ static/js/model.js
@@ -2,6 +2,17 @@ let baseUrl = "";
 
 window.addEventListener("load", function(){
 	baseUrl = window.location.origin;
-	console.log(data["id"]);
-	console.log(baseUrl);
+	getHTML();
 }, false)
+
+function getHTML(){
+	let thisBody = null;
+	fetch(baseUrl +"/theorie/" + data["id"])
+	.then(response => response.json())
+	.then(body => gotBody(body));
+}
+
+function gotBody(body){
+	let container = document.getElementById('modellBody');
+	container.innerHTML = body["html"];
+}
