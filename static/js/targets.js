e6d06be5c148f71d884b25c2ef3d56c9ec846997
AdiJoost <adi.joost@gmail.com>
2021-08-26 15:00:38 +0200

    target.html now can filter targets by first number

--- static/js/targets.js
+++ static/js/targets.js
@@ -1,8 +1,11 @@
 let baseUrl = "";
+let buttons = [false, false, false, false, false, false, false];
+let buttonsLength = 7;
 
 window.addEventListener("load", function(){
 	baseUrl = window.location.origin;
 	loadAllTargets();
+	setupButtons();
 }, false)
 
 function loadAllTargets(){
@@ -52,4 +55,48 @@ function displayTargets(target, description){
 	targetNumber.addEventListener("click", function(){
 		targetExplain.classList.toggle("hiden");
 	}, false);
+}
+
+function setupButtons(){
+	for (let i = 0; i < buttonsLength; i++){
+		let button = document.getElementById('cb' + i);
+		button.addEventListener("click", function(){
+			toggleButton(button, i);
+		}, false);
+	}
+}
+
+function toggleButton(button, index){
+	button.classList.toggle("cklicked");
+	buttons[index] = !buttons[index];
+	clearBody();
+	getTargets();
+	console.log(buttons);
+}
+
+function getTargets(){
+	fetch(baseUrl + "/all_targets",{
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"types": getStates()
+							})
+	})
+	.then(response => response.json())
+	.then(body => gotBody(body));
+}
+
+function getStates(){
+	return_value = "";
+	for (let i = 0; i < buttonsLength; i++){
+		if (buttons[i] == true){
+			return_value += "1";
+		} else {
+			return_value += "0";
+		}
+	}
+	return return_value;
+
 }
